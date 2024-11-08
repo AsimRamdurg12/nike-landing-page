da7db5db639e1be921c3d08f55d63206c6ee238f
AsimRamdurg12 <asimramdurg12@gmail.com>
2024-11-08 18:58:32 +0530

    init

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,11 @@
-import { hamburger } from "../assets/icons";
+import { useState } from "react";
+import { close, hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
@@ -21,14 +24,33 @@ const NavBar = () => {
             </li>
           ))}
         </ul>
-        <div>
+
+        <div className="lg:hidden flex flex-1 justify-end items-center">
           <img
-            src={hamburger}
+            src={toggle ? close : hamburger}
             alt="menu"
-            height={25}
-            width={25}
-            className="hidden max-lg:block"
+            className="h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
           />
+        </div>
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl z-10`}
+        >
+          <ul className="list-none border-2 border-black p-5 rounded-lg flex justify-end items-start flex-col mt-0 gap-4">
+            {navLinks.map((item) => (
+              <li
+                key={item.label}
+                className={` font-poppins font-medium cursor-pointer text-[16px]`}
+                onClick={() => {
+                  setToggle(!toggle);
+                }}
+              >
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
         </div>
       </nav>
     </header>
