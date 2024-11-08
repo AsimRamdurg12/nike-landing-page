20e4295fda94022656d0d12d4aff9b3053afc4c1
AsimRamdurg12 <asimramdurg12@gmail.com>
2024-11-08 16:18:20 +0530

    init

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex justify-center items-center gsp-2 px-7 py-4 border font-montserrat leading-none text-lg bg-coral-red rounded-full text-white">
+    <button
+      className={`flex justify-center items-center gsp-2 px-7 py-4 border font-montserrat leading-none text-lg ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
-      <img src={iconURL} alt="arrow" className="ml-2 w-5 h-5 rounded-full" />
+      {iconURL && (
+        <img src={iconURL} alt="arrow" className="ml-2 w-5 h-5 rounded-full" />
+      )}
     </button>
   );
 };
